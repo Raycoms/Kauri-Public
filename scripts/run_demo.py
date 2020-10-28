4fe5366e364a76a0c9eaf0a8d4d07f76e1a24f6b
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-10-28 16:23:40 +0000

    attenot

--- scripts/run_demo.py
+++ scripts/run_demo.py
@@ -7,8 +7,20 @@ import paramiko
 if __name__ == "__main__":
     ipSet = [l.strip() for l in open("ips", 'r').readlines()]
     ips = []
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    port = 22
+    user = "root"
+    password = ""
+
     for ipEl in ipSet:
         ipElSet = ipEl.split(" ")
+
+        ssh.connect(ipElSet[0],port,user,password,timeout = 10)
+        command = "killall hostuff-app"
+        ssh.close()
+
         print(ipElSet)
         for x in range(int(ipElSet[1])):
             ips.append(ipElSet[0])
@@ -16,24 +28,17 @@ if __name__ == "__main__":
     prefix = 'hotstuff.gen'
 
     print(ips)
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     i = 0
     replicas = []
     for ip in ips:
-        port = 22
-        user = "root"
-        password = ""
+
         # Establish a connection
         ssh.connect(ip,port,user,password,timeout = 10)
         command = "cd test/libhotstuff && ./examples/hotstuff-app --conf ./hotstuff.gen-sec{}.conf > log{} &".format(i, i)
         # Enter the Linux command
-        stdin,stdout,stderr = ssh.exec_command(command)
         # Output command execution results
-        result = stdout.read()
         print("finished!")
-        print(result)
         ssh.close()
         i+=1
 
