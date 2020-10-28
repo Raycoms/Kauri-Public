5829ce028d201f85e14839922eda98dd97d66db6
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-10-28 17:08:03 +0000

    try to cd and then run

--- scripts/run_demo.py
+++ scripts/run_demo.py
@@ -4,6 +4,7 @@ import itertools
 import argparse
 import paramiko
 import time
+import os
 
 if __name__ == "__main__":
     ipSet = [l.strip() for l in open("ips", 'r').readlines()]
@@ -18,10 +19,14 @@ if __name__ == "__main__":
     for ipEl in ipSet:
         ipElSet = ipEl.split(" ")
 
-        ssh.connect(ipElSet[0],port,user,password,timeout = 10)
-        command = "killall hotstuff-app"
-        ssh.exec_command(command)
-        ssh.close()
+        if ipElSet[0] == "127.0.0.1":
+            print("sys")
+            os.system("killall hotstuff-app")
+        else:
+            ssh.connect(ipElSet[0],port,user,password,timeout = 10)
+            command = "killall hotstuff-app"
+            ssh.exec_command(command)
+            ssh.close()
 
         print(ipElSet)
         for x in range(int(ipElSet[1])):
@@ -35,15 +40,23 @@ if __name__ == "__main__":
     for ipEl in ipSet:
         ipElSet = ipEl.split(" ")
 
-        ssh.connect(ipElSet[0],port,user,password,timeout = 10)
-        print("Run at {}".format(ipElSet[0]))
-        for x in range(int(ipElSet[1])):
-            command = "cd test/libhotstuff && ./examples/hotstuff-app --conf ./hotstuff.gen-sec{}.conf > log{} 2>&1 &".format(i, i)
-            print(command)
-            ssh.exec_command(command)
-            i+=1
+        if ipElSet[0] == "127.0.0.1":
+            for x in range(int(ipElSet[1])):
+                command = "./examples/hotstuff-app --conf ./hotstuff.gen-sec{}.conf > log{} 2>&1 &".format(i, i)
+                print(command)
+                os.system(command)
+                i+=1
+        else:
+            ssh.connect(ipElSet[0],port,user,password,timeout = 10)
+            ssh.exec_command("cd test/libhotstuff")
+            print("Run at {}".format(ipElSet[0]))
+            for x in range(int(ipElSet[1])):
+                command =  "./examples/hotstuff-app --conf ./hotstuff.gen-sec{}.conf > log{} 2>&1 &".format(i, i)
+                print(command)
+                ssh.exec_command(command)
+                i+=1
 
-        ssh.close()
+            ssh.close()
 
 
     time.sleep( 60 )
@@ -53,8 +66,12 @@ if __name__ == "__main__":
     for ipEl in ipSet:
         ipElSet = ipEl.split(" ")
 
-        ssh.connect(ipElSet[0],port,user,password,timeout = 10)
-        command = "killall hotstuff-app"
-        ssh.exec_command(command)
-        ssh.close()
+        if ipElSet[0] == "127.0.0.1":
+            print("sys")
+            os.system("killall hotstuff-app")
+        else:
+            ssh.connect(ipElSet[0],port,user,password,timeout = 10)
+            command = "killall hotstuff-app"
+            ssh.exec_command(command)
+            ssh.close()
 
