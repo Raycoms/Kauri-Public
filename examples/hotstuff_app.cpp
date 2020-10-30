492644e10af18adc1ed60521f0c84f52f640b558
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-10-30 12:56:17 +0000

    fix

--- examples/hotstuff_app.cpp
+++ examples/hotstuff_app.cpp
@@ -63,7 +63,7 @@ using hotstuff::MsgRespCmd;
 using hotstuff::get_hash;
 using hotstuff::promise_t;
 
-using HotStuff = hotstuff::HotStuffSe;
+using HotStuff = hotstuff::HotStuffAgg;
 
 class HotStuffApp: public HotStuff {
     double stat_period;
