215b5b87316b69f5a1cf4fe0ee707916438ccc81
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-10-30 09:05:34 +0000

    secp

--- examples/hotstuff_app.cpp
+++ examples/hotstuff_app.cpp
@@ -63,7 +63,7 @@ using hotstuff::MsgRespCmd;
 using hotstuff::get_hash;
 using hotstuff::promise_t;
 
-using HotStuff = hotstuff::HotStuffAgg;
+using HotStuff = hotstuff::HotStuffSecp256k1;
 
 class HotStuffApp: public HotStuff {
     double stat_period;
