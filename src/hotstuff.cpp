b7d9c36f3c179d56adf9f7af31216760e074ee5e
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-11-04 16:07:32 +0000

    add small sleep too

--- src/hotstuff.cpp
+++ src/hotstuff.cpp
@@ -655,6 +655,7 @@ void HotStuffBase::start(std::vector<std::tuple<NetAddr, pubkey_bt, uint256_t>> 
     for (const PeerId& peer : newPeers)
     {
         pn.conn_peer(peer);
+        usleep(1000);
     }
 
     std::cout << " total children: " << children.size() << std::endl;
