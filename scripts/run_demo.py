fd070bbd74aae271f7242fc7a3bbd65122b2973a
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-11-20 15:57:41 +0000

    moar

--- scripts/run_demo.py
+++ scripts/run_demo.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
 
     for j in range(1, 10):
         print("Starting Client!")
-        ssh.exec_command("cd test/libhotstuff && ./examples/hotstuff-client --idx {} --iter -2000 --max-async 1000 > clientlog{} 2>&1 &".format(j, j))
+        ssh.exec_command("cd test/libhotstuff && ./examples/hotstuff-client --idx {} --iter -500 --max-async 500 > clientlog{} 2>&1 &".format(j, j))
 
     time.sleep( 300 )
 
