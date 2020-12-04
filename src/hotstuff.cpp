ec16155288645b5ab4db6143c0ac6856b66a3123
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-12-04 08:26:33 +0000

    bls tree

--- src/hotstuff.cpp
+++ src/hotstuff.cpp
@@ -761,7 +761,7 @@ void HotStuffBase::start(std::vector<std::tuple<NetAddr, pubkey_bt, uint256_t>> 
 
                         if ( b_piped == nullptr && current != get_genesis() ) {
 
-                            if (false && ((current_time.tv_sec - last_block_time.tv_sec) * 1000000 + current_time.tv_usec - last_block_time.tv_usec) / 1000 < config.piped_latency) {
+                            if (((current_time.tv_sec - last_block_time.tv_sec) * 1000000 + current_time.tv_usec - last_block_time.tv_usec) / 1000 < config.piped_latency) {
                                 piped_submitted = false;
                             }
                             else {
