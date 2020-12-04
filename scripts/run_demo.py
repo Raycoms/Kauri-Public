fb1738ebf8151153d6920d17bfe1c9c04aad76ec
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-12-04 09:24:07 +0000

    much  less clients

--- scripts/run_demo.py
+++ scripts/run_demo.py
@@ -86,9 +86,9 @@ if __name__ == "__main__":
 
     time.sleep(3)
 
-    for j in range(1, 50):
+    for j in range(1, 20):
         print("Starting Client!")
-        ssh.exec_command("cd test/libhotstuff && ./examples/hotstuff-client --idx {} --iter -750 --max-async 1000 > clientlog{} 2>&1 &".format(1, j))
+        ssh.exec_command("cd test/libhotstuff && ./examples/hotstuff-client --idx {} --iter -100 --max-async 500 > clientlog{} 2>&1 &".format(1, j))
 
     time.sleep( 300 )
 
