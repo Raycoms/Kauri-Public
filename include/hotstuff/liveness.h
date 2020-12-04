737c179d432af49c144fd35d04f6191626089707
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-12-04 17:10:17 +0000

    enable pipe

--- include/hotstuff/liveness.h
+++ include/hotstuff/liveness.h
@@ -144,8 +144,6 @@ class PMWaitQC: public virtual PaceMaker {
         if (!pending_beats.empty())
         {
             if (locked) {
-                if (true)
-                    return;
                 struct timeval current_time;
                 gettimeofday(&current_time, NULL);
                 if (((hsc->b_piped == nullptr && !hsc->piped_submitted)
