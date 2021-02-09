dd7e1f3893cf9181e496ecfa84607ce0ae39fdde
Ray  Neiheiser <ray.neiheiser@gmail.com>
2021-02-09 19:51:10 +0000

    inc again

--- scripts/gen_conf.py
+++ scripts/gen_conf.py
@@ -71,8 +71,8 @@ if __name__ == "__main__":
 
     main_conf.write("proposer = {}\n".format(0))
     main_conf.write("fan-out = {}\n".format(3))
-    main_conf.write("piped_latency = {}\n".format(20))
-    main_conf.write("async_blocks = {}\n".format(5))
+    main_conf.write("piped_latency = {}\n".format(10))
+    main_conf.write("async_blocks = {}\n".format(10))
 
     for r in zip(replicas, keys, tls_keys2[:len(keys)], itertools.count(0)):
         main_conf.write("replica = {}, {}, {}\n".format(r[0], r[1][0], r[2][2]))
