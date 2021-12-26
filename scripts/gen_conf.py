666cd35eb4014e0a6d1842b93cc69c2240358805
Ray  Neiheiser <ray.neiheiser@gmail.com>
2021-12-26 14:11:18 -0300

    0.5

--- scripts/gen_conf.py
+++ scripts/gen_conf.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
     main_conf.write("piped_latency = {}\n".format(args.pipelatency))
     main_conf.write("async_blocks = {}\n".format(args.pipedepth))
     main_conf.write("base-timeout = {}\n".format(2.0))
-    main_conf.write("prop-delay = {}\n".format(1.0))
+    main_conf.write("prop-delay = {}\n".format(0.5))
 
     for r in zip(replicas, keys, tls_keys2[:len(keys)], itertools.count(0)):
         main_conf.write("replica = {}, {}, {}\n".format(r[0], r[1][0], r[2][2]))
