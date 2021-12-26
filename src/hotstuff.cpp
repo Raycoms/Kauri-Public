abe032a39e60f3d500e8411290750edbdf3bc6c8
Ray  Neiheiser <ray.neiheiser@gmail.com>
2021-12-26 15:48:24 -0300

    I can adjust this back too

--- src/hotstuff.cpp
+++ src/hotstuff.cpp
@@ -792,11 +792,11 @@ ReplicaID HotStuffBase::calcTree(std::vector<std::tuple<NetAddr, pubkey_bt, uint
         failures++;
 
         // 9 times
-        if (failures < 9 && fanout < global_replicas.size()) {
+        if (failures < 9) {
             //we actually do this m+1 times (depending on the depth right))
             std::rotate(global_replicas.begin(), global_replicas.begin() + fanout + 1, global_replicas.end());
         }
-        else if (failures == 9 && !faulty.empty() && fanout < global_replicas.size()) {
+        else if (failures == 9 && !faulty.empty()) {
             std::rotate(global_replicas.begin(), global_replicas.begin() + fanout + 2, global_replicas.end());
 
             auto cert_hash = std::move(std::get<2>(original_replicas.at(faulty.at(0))));
