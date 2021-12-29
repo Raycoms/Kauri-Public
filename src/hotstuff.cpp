1d6a82ca676a8f68ada52622e66e6e6fca3ff788
Ray  Neiheiser <ray.neiheiser@gmail.com>
2021-12-29 17:05:52 -0300

    try

--- src/hotstuff.cpp
+++ src/hotstuff.cpp
@@ -751,7 +751,7 @@ void HotStuffBase::do_vote(Proposal prop, const Vote &vote) {
 
         if (childPeers.empty()) {
             HOTSTUFF_LOG_PROTO("send vote");
-            pn.send_msg(MsgVote(vote), peers[proposer]);
+            pn.send_msg(MsgVote(vote), parentPeer);
         } else {
             block_t blk = get_delivered_blk(vote.blk_hash);
             if (blk->self_qc == nullptr)
