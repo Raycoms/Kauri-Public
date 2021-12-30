496c7ae24f9ab7bbcb9b31231356db24320ce1df
Ray  Neiheiser <ray.neiheiser@gmail.com>
2021-12-30 08:11:09 -0300

    attempt

--- include/hotstuff/liveness.h
+++ include/hotstuff/liveness.h
@@ -298,8 +298,8 @@ public:
     void set_proposer(TimerEvent &) {
 
         timeout += 0.5;
-        if (timeout > 16.0) {
-            timeout = 16.0;
+        if (timeout > 10.0) {
+            timeout = 10.0;
         }
         HOTSTUFF_LOG_PROTO("-------------------------------");
         HOTSTUFF_LOG_PROTO("Timeout reached!!!");
